5b80ef32208a0a7175a9957b7640d9f821d2481e
myhency <hency.yeo@gmail.com>
2023-01-19 14:39:04 +0900

    feat: draft screens 추가

---
+++ src/screens/FavoriteNewsListScreen.tsx
@@ -0,0 +1,12 @@
+import { View } from "react-native";
+import { Header } from "../components/Header/Header";
+
+export const FavoriteNewsListScreen = () => {
+    return (
+        <View>
+            <Header>
+                <Header.Title title="Favorite News List" />
+            </Header>
+        </View>
+    );
+};
---
+++ src/screens/NewsDetail.tsx
@@ -0,0 +1,12 @@
+import { View } from "react-native";
+import { Header } from "../components/Header/Header";
+
+export const NewsDetailScreen = () => {
+    return (
+        <View>
+            <Header>
+                <Header.Title title="News Detail" />
+            </Header>
+        </View>
+    );
+};
