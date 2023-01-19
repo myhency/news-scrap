3d31677fd0bbce7d2f1ab277bfd2ec85dd4395a3
myhency <hency.yeo@gmail.com>
2023-01-19 14:31:15 +0900

    feat: component 추가

---
+++ src/components/SingleLineInput.tsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import {
+    KeyboardTypeOptions,
+    StyleProp,
+    TextInput,
+    TextStyle,
+    View,
+} from "react-native";
+
+interface Props {
+    value: string;
+    placeholder?: string;
+    style?: StyleProp<TextStyle>;
+    fontSize?: number;
+    keyboardType?: KeyboardTypeOptions;
+    onChangeText: (text: string) => void;
+    onSubmitEditing: () => void;
+}
+
+export const SingleLineInput = ({
+    value,
+    placeholder,
+    style,
+    fontSize,
+    keyboardType,
+    onChangeText,
+    onSubmitEditing,
+}: Props) => {
+    const [focused, setFocused] = useState(false);
+
+    return (
+        <View
+            style={{
+                alignSelf: "stretch",
+                paddingHorizontal: 12,
+                paddingVertical: 8,
+                borderRadius: 4,
+                borderWidth: 1,
+                borderColor: focused ? "black" : "gray",
+            }}
+        >
+            <TextInput
+                keyboardType={keyboardType ?? "default"}
+                autoCorrect={false}
+                value={value}
+                onChangeText={onChangeText}
+                placeholder={placeholder}
+                style={[style, { fontSize: fontSize ?? 20 }]}
+                onFocus={() => setFocused(true)}
+                onBlur={() => setFocused(false)}
+                onSubmitEditing={onSubmitEditing}
+            />
+        </View>
+    );
+};
